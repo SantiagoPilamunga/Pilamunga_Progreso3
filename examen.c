4b341d5e53e7dd78b3b327919338390ad2be4798
Santiago <santiago.pilamunga@udla.edu.ec>
2024-01-23 14:18:46 -0500

    hacemos que seleccione una opcion

--- examen.c
+++ examen.c
@@ -12,21 +12,31 @@ int main(int argc, char const *argv[])
 {
     int i;
     struct alumno a[200];
-    switch (i)
+    do
     {
-    case 1:
-        printf("Ingresar alumnos");
-        break;
-    case 2:
-        printf("Mostrar alumnos");
-        break;
-    case 3:
-        printf("Gracias por usar el programa");
-        break;
-    default:
-        printf("La opcion no existe");
-        break;
-    }
+        printf("Seleccione una opcion: ");
+        printf("1. Ingresar un nuevo alumno");
+        printf("2. Mostrar los alumnos");
+        printf("3. Salir del programa");
+        scanf("%d", &i);
+        switch (i)
+        {
+        case 1:
+            printf("Ingresar alumnos");
+            break;
+        case 2:
+            printf("Mostrar alumnos");
+            break;
+        case 3:
+            printf("Gracias por usar el programa");
+            break;
+        default:
+            printf("La opcion no existe");
+            break;
+        }
+    } while (i!=3);
+    
+        
 
     return 0;
 }
