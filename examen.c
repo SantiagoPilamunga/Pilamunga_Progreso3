fc26920dced2a1207a3e66ad033ec6f5bcc58405
Santiago <santiago.pilamunga@udla.edu.ec>
2024-01-23 14:09:33 -0500

    Creamos la cadena

--- examen.c
+++ examen.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+struct alumno
+{
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char materia[50];
+    float nota;
+};
+
 int main(int argc, char const *argv[])
 {
     
